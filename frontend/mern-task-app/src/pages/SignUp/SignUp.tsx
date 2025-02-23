626b8389e56400feb02ab124285406c5234357fb
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-23 15:53:17 +0530

    Signup page created

--- frontend/mern-task-app/src/pages/SignUp/SignUp.tsx
+++ frontend/mern-task-app/src/pages/SignUp/SignUp.tsx
@@ -1,10 +1,85 @@
-import React from 'react';
+import {useState} from 'react';
+import {Link} from "react-router-dom";
+import {validateEmail} from "../../utils/helper.tsx";
+import Navbar from "../../components/Navbar/Navbar.tsx";
+import PasswordInput from "../../components/Input/PasswordInput.tsx";
 
 const SignUp = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        if (!name) {
+            setError("Please enter your name");
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if (!password) {
+            setError("Please enter the password");
+            return;
+        }
+
+        setError('')
+
+        // SignUp API Calls
+
+    };
+
     return (
-        <div>
-            Signup
-        </div>
+        <>
+            <Navbar />
+
+            <div className="flex items-center justify-center mt-28">
+                <div className="w-96 border border-gray-300 rounded-lg bg-white px-7 py-10">
+                    <form onSubmit={handleSignUp}>
+                        <h4 className="text-2xl mb-7">Sign Up</h4>
+
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            className="input-box"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            className="input-box"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+
+                        <PasswordInput
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+
+                        {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+                        <button type="submit" className="btn-primary w-full">Create Account</button>
+
+                        <p className="text-sm text-center mt-4">
+                            Already have an account?{" "}
+                            <Link to="/login" className="font-medium text-blue-500 underline hover:text-blue-600">
+                                Login
+                            </Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
