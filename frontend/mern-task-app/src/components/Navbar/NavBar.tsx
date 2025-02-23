0d5060f9b36ee582b770cf429854382848c3e6da
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-23 13:21:54 +0530

    Simple Navbar created

--- frontend/mern-task-app/src/components/Navbar/NavBar.tsx
+++ frontend/mern-task-app/src/components/Navbar/NavBar.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div>
-            <div className="pg-white flex justify-between px-6 py-2 drop-shadow">
-                <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+            <div className="bg-white flex justify-between px-6 py-2 drop-shadow-md">
+                <h2 className="text-xl font-medium text-black py-2">Goal Grid</h2>
             </div>
         </div>
     );
