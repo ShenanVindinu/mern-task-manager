ad85bd4aef5c7bc2ab9db163defa87b7a7c023b1
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-23 20:36:35 +0530

    AddEditNote Component added as a Modal

--- frontend/mern-task-app/src/pages/Home/Home.tsx
+++ frontend/mern-task-app/src/pages/Home/Home.tsx
@@ -2,8 +2,19 @@
 import Navbar from "../../components/Navbar/Navbar.tsx";
 import NoteCard from "../../components/Cards/NoteCard.tsx";
 import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes.tsx";
+import {useState} from "react";
+import Modal from "react-modal";
 
 const Home = () => {
+
+    const [openAddEditModal, setOpenAddEditModal] = useState({
+        isShown: false,
+        type: "add",
+        data: null,
+    });
+
+
     return (
         <>
             <Navbar />
@@ -26,10 +37,31 @@ const Home = () => {
 
             <button
                 className="w-16 h-16 flex items-center justify-center rounded-full bg-blue-500 hover:bg-blue-600 absolute right-10 bottom-10 transition-colors z-[9999]"
-                onClick={() => {}}
+                onClick={() => {
+                    setOpenAddEditModal({ isShown: true, type: "add", data: null });
+                }}
             >
                 <MdAdd className="text-white text-3xl" />
             </button>
+
+            <Modal
+                isOpen={openAddEditModal.isShown}
+                onRequestClose={() => {}}
+                style={{
+                    overlay: {
+                        backgroundColor: "rgba(0,0,0,0.2)",
+                    },
+                }}
+                contentLabel="Example Modal"
+                className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-auto scrollbar-hide"
+            >
+                <AddEditNotes
+                    type={openAddEditModal.type}
+                    noteData={openAddEditModal.data}
+                    onClose={() => {
+                        setOpenAddEditModal({isShown: false, type: "add", data: null});
+                    }} showToastMessage={undefined} getAllNotes={undefined}                />
+            </Modal>
 
 
 
