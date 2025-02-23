d9744692e6995afaa58a9599ddfd8c3fd39669ad
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-23 17:25:56 +0530

    Note Card Created

--- frontend/mern-task-app/src/pages/Home/Home.tsx
+++ frontend/mern-task-app/src/pages/Home/Home.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+
 import Navbar from "../../components/Navbar/Navbar.tsx";
+import NoteCard from "../../components/Cards/NoteCard.tsx";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+
+            <div className="container mx-auto">
+                <div className="grid grid-cols-3 gap-4 mt-8 ml-10">
+                    <NoteCard
+                        title="Do Lawn Mowing"
+                        content="lorem ipsum dolor sit amet"
+                        date="2024/01/05"
+                        tags="#sbkcabnscxl"
+                        isPinned={true}
+                        onEdit={() => {}}
+                        onDelete={() => {}}
+                        onPinNote={() => {}}
+                    />
+                </div>
+            </div>
+
+
+
         </div>
     );
 };
