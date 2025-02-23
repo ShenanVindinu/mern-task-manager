2494638e04ca591bc0bd0da92920826d032ee99a
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-23 16:35:42 +0530

    profilke info and searchbar added to Navbar.tsx then navbar added to Home

--- frontend/mern-task-app/src/components/Navbar/Navbar.tsx
+++ frontend/mern-task-app/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import { useState } from 'react';
+import ProfileInfo from "../Cards/ProfileInfo.tsx";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar.tsx";
 
-const Navbar = () => {
+const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        localStorage.clear();
+        navigate("/login");
+    };
+
+    const handleSearch = () => {
+        if(searchQuery){
+            onSearchNote(searchQuery)
+        }
+    };
+
+    const onClearSearch = ()=>{
+        handleClearSearch()
+        setSearchQuery("")
+    }
+
     return (
-        <div>
-            <div className="bg-white flex justify-between px-6 py-2 drop-shadow-md">
-                <h2 className="text-xl font-medium text-black py-2">Goal Grid</h2>
-            </div>
+
+        <div className="bg-white flex justify-between px-6 py-2 drop-shadow-md">
+            <h2 className="text-xl font-medium text-black py-2">Goal Grid</h2>
+
+            <SearchBar
+                value={searchQuery}
+                onChange={({ target }) => {
+                    setSearchQuery(target.value);
+                }}
+                handleSearch={handleSearch}
+                onClearSearch={onClearSearch}
+            />
+
+            <ProfileInfo onLogout={onLogout} />
         </div>
+
     );
 };
 
--- frontend/mern-task-app/src/pages/Home/Home.tsx
+++ frontend/mern-task-app/src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from "../../components/Navbar/Navbar.tsx";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Navbar />
         </div>
     );
 };
