5d8d099d58d8edbedaa51ff25be15dc9c212be6b
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-23 13:12:34 +0530

    jsx converted to tsx.

---
+++ frontend/mern-task-app/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.tsx";
+import Login from "./pages/Login/Login.tsx";
+import SignUp from "./pages/SignUp/SignUp.tsx";
+
+const routes = (
+    <Router>
+        <Routes>
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+        </Routes>
+    </Router>
+);
+
+const App = () => {
+    return (
+        <div>
+            {routes}
+        </div>
+    );
+};
+
+export default App;
