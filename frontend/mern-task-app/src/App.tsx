1dfb3e2b483c150bbc7c81efa3e2e446962c3cb4
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-26 12:39:04 +0530

    unused react import removed

--- frontend/mern-task-app/src/App.tsx
+++ frontend/mern-task-app/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
 import Login from "./pages/Login/Login.tsx";
